b6fa553524dc0f60dc92ef258f055f61761da905
Viktor Zotov <ZotovV777@yandex.ru>
2022-10-29 20:35:24 +0300

    error corection

--- brain_games/games/is_even.py
+++ brain_games/games/is_even.py
@@ -1,16 +1,16 @@
 from random import randint
 
 
-game_task = 'Answer \'yes\' if the number is even, otherwise answer \'no\'.'
+game_task = 'Answer \"yes\" if the number is even, otherwise answer \"no\".'
 
 
 def even():
     random_number = randint(1, 100)
     question = (f'{random_number}')
     if random_number % 2 == 0:
-        exp = 'yes'
+        exp = "yes"
     if random_number % 2 == 1:
-        exp = 'no'
+        exp = "no"
     return [question, exp]
 
 
